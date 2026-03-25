ddda5761493dfa1a5888c2b0c460cfe233b6f4e1
mirzohid <mmahmutaliyev411@gmail.com>
2026-03-25 19:41:31 +0500

    j

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
+import LayoutWrapper from "@/components/LayoutWrapper";
 import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
